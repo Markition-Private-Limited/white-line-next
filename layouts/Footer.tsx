fba2d60fda71a79dcc5545f1e2e3cd98c3a3b37b
hammad khalid <hammadkhalidpk17@gmail.com>
2026-09-02 20:02:36 +0500

    socials conncted
    socials connected

--- layouts/Footer.tsx
+++ layouts/Footer.tsx
@@ -112,7 +112,7 @@ export default function Footer() {
               <SocialIcon href="https://www.facebook.com/profile.php?id=61593680866369" label="Facebook">
                 <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" /></svg>
               </SocialIcon>
-              <SocialIcon href="#" label="Twitter">
+              <SocialIcon href="https://x.com/whiteline_int" label="Twitter">
                 <svg width="15" height="15" viewBox="0 0 24 24" fill="currentColor"><path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z" /></svg>
               </SocialIcon>
               <SocialIcon href="https://www.linkedin.com/company/whitelineglobal" label="LinkedIn">
